6435da48a3ebc000cf5bd6b25dfa67a5f56fe87f
gabrielmellooliveira <gabrielmello3011@gmail.com>
2020-03-27 21:30:12 -0300

    Fix frontend items

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -13,7 +13,7 @@ export default function Register() {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [Whatsapp, setWhatsapp] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
     const [city, setCity] = useState('');
     const [uf, setUf] = useState('');
 
